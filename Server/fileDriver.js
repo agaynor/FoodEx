73b292e3cc9918f656466273e31b846faccd8c58
Adam Gaynor <adamsg9425@gmail.com>
2016-02-28 16:31:42 -0600

    Commented fileDriver.js

--- Server/fileDriver.js
+++ Server/fileDriver.js
@@ -55,11 +55,12 @@ FileDriver.prototype.handleGet = function(req, res) {
 	}
 };
 
-
+//Helper method to save a file object to the db
 FileDriver.prototype.save = function(obj, callback) {
 	this.getCollection(function(error, the_collection) {
 		if(error) callback(error);
 		else{
+			//Set file created at date
 			obj.created_at = new Date();	
 			the_collection.insert(obj, function(){
 				callback(null, obj);
@@ -68,28 +69,43 @@ FileDriver.prototype.save = function(obj, callback) {
 	});
 };
 
+//Saves a new fileobject and returns the id
 FileDriver.prototype.getNewFileId = function(newobj, callback) {
+	//Uses helper method above to save fileobject
 	this.save(newobj, function(err, obj){
 		if(err) { callback(err); }
-		else { callback(null, obj._id); }
+		else {
+			//Passes id back in callback function 
+			callback(null, obj._id); 
+		}
 	});
 };
 
+//Handles a fileupload from the server
 FileDriver.prototype.handleUploadRequest = function(req, res) {
+	//Gets content-type of the file
 	var ctype = req.get('content-type');
+	//Get the extension of the file
 	var ext = ctype.substr(ctype.indexOf('/')+1);
 	if(ext) {ext = '.' + ext;} else{ext = '';}
+	//Create a new file entry in the database with specified extension and content-type
 	this.getNewFileId({'content-type':ctype, 'ext':ext}, function(err,id){
 		if(err){res.send(400,err);}
 		else{
+			//Set filename to the fileid in the db and the extension appended
 			var filename = id+ext;
+			//Set the path to the file
 			filePath = __dirname + '/uploads/' + filename;
 			
+			//Create a writestream to the path where file will be saved
 			var writeable = fs.createWriteStream(filePath);
+			//Pipe the file to the stream
 			req.pipe(writeable);
+			//Listen for end of pipe, send success with fileid
 			req.on('end', function(){
 				res.send(201, {'_id':id});
 			});
+			//Listen for stream error, send error response
 			writeable.on('error', function(err) {
 				res.send(500, err);
 			});
@@ -97,4 +113,5 @@ FileDriver.prototype.handleUploadRequest = function(req, res) {
 	});
 };
 
+//Exports the CollectionDriver so it can be used by main server code		
 exports.FileDriver = FileDriver;
