7b13b82617405abf84685fe25fce7ffad6225eca
Adam Gaynor <adamsg9425@gmail.com>
2016-02-28 15:24:21 -0600

    Initial Commit

---
+++ FoodEx/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FoodEx
+//
+//  Created by Adam Gaynor on 2/28/16.
+//  Copyright © 2016 FoodEx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FoodEx/SecondViewController.m
@@ -0,0 +1,27 @@
+//
+//  SecondViewController.m
+//  FoodEx
+//
+//  Created by Adam Gaynor on 2/28/16.
+//  Copyright © 2016 FoodEx. All rights reserved.
+//
+
+#import "SecondViewController.h"
+
+@interface SecondViewController ()
+
+@end
+
+@implementation SecondViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
