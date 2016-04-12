a6d8f7b70635f5900d56608bdd46c9c97595d2f0
Adam Gaynor <adamsg9425@gmail.com>
2016-04-12 16:19:43 -0500

    testing

--- FoodEx/FoodRequests.m
+++ FoodEx/FoodRequests.m
@@ -128,6 +128,7 @@ static NSString* const kDeliveryAccept = @"deliveryAccept";
     NSURLSessionDataTask* dataTask = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) { //5
         if (error == nil) {
             NSArray* responseArray = [NSJSONSerialization JSONObjectWithData:data options:0 error:NULL]; //6
+            NSLog(@"%ul", [responseArray count]);
             [self.requests removeAllObjects];
             [self parseAndAddLocations:responseArray toArray:self.requests];
             
